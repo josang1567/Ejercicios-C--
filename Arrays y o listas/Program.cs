e09d1a2b9f1150eb34aa6ee45e0741fa350bf519
José Ángel Marín Moreno <joseangelmarin1234@gmail.com>
2023-10-18 19:30:02 +0200

    Array o listas

---
+++ Arrays y o listas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arrays_y_o_listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
